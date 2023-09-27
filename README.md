8badddb34a5ab9fce3174002dfb3ac8a2eeeb7bc
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2023-09-27 17:04:30 -0400

    feat: Import of sidetree-core-go client code
    Imported code from sidetree-core-go that is required by clients to create, update, deactivate, recover and resolve DIDs. The server-side code was not imported.
    
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sidetree-go
+This library implements core components required to implement `Sidetree Protocol: <https://github.com/decentralized-identity/sidetree/blob/master/docs/protocol.md>`_
+
+## Contributing
+Thank you for your interest in contributing. Please see our [community contribution guidelines](https://github.com/trustbloc/community/blob/main/CONTRIBUTING.md) for more information.
+
+## License
+Apache License, Version 2.0 (Apache-2.0). See the [LICENSE](LICENSE) file.
---
+++ pkg/document/resolution_test.go
@@ -0,0 +1,27 @@
+/*
+Copyright Gen Digital Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package document
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/trustbloc/sidetree-go/pkg/api/operation"
+)
+
+func TestGetOptions(t *testing.T) {
+	const verTime = "2021-05-10T17:00:00Z"
+	const verID = "ver"
+
+	opts, err := GetResolutionOptions(WithAdditionalOperations([]*operation.AnchoredOperation{{Type: "create"}}),
+		WithVersionID(verID), WithVersionTime(verTime))
+	require.NoError(t, err)
+	require.Equal(t, 1, len(opts.AdditionalOperations))
+	require.Equal(t, verID, opts.VersionID)
+	require.Equal(t, verTime, opts.VersionTime)
+}
